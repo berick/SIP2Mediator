5cbd38ba1c2b6a43804f68e35f404a4362f3b72f
Bill Erickson <berickxx@gmail.com>
2022-05-16 16:17:37 -0400

    Add a root module for packaging
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

---
+++ lib/SIP2Mediator.pm
@@ -0,0 +1,23 @@
+package SIP2Mediator;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+SIP2Mediator
+
+=head1 SYNOPSIS
+
+SIP2 server which relays SIP2 messages to an HTTP backend for processing,
+then returns the responses from the HTTP backend back to the SIP client.
+
+=head1 AUTHOR
+
+Bill Erickson <berickxx@gmail.com>
+  
+=head1 COPYRIGHT
+   
+Copyright 2021-2022 by King County Library System <kcls.org>
+
